ca45d0c817a1f9337a00a90dc79a7a3b65286bc0
jchaffin87 <jchaffin87@gmail.com>
2018-08-27 22:04:20 -0400

    second test written for getTotalDistanceDriven

--- src/test/java/com/jchaffin/rootcodechallenge/DriverTest.java
+++ src/test/java/com/jchaffin/rootcodechallenge/DriverTest.java
@@ -15,10 +15,24 @@ public class DriverTest {
 	}
 
 	@Test
-	public void getTotalMilesDrivenReturnsDistanceOfOneTripInDriversTripsArray() {
+	public void getTotalMilesDrivenReturnsDistanceOfOneTripInDriversTripsArrayAsInt() {
 		Trip testTrip = new Trip("Test", null, null, 10.1);
 		testDriver.addTrip(testTrip);
 		assertEquals((int) Math.round(testTrip.getDistance()), testDriver.getTotalDistanceDriven());
+	}
+
+	@Test
+	public void getTotalMilesDrivenReturnsSumOfAllTripDistancesInTripsArray() {
+		Trip testTrip1 = new Trip("Test", null, null, 10.1);
+		testDriver.addTrip(testTrip1);
+		Trip testTrip2 = new Trip("Test", null, null, 10.5);
+		testDriver.addTrip(testTrip2);
+		Trip testTrip3 = new Trip("Test", null, null, 10.1);
+		testDriver.addTrip(testTrip3);
+		int tripDistance1 = (int) Math.round(testTrip1.getDistance());
+		int tripDistance2 = (int) Math.round(testTrip2.getDistance());
+		int tripDistance3 = (int) Math.round(testTrip3.getDistance());
+		assertEquals(tripDistance1 + tripDistance2 + tripDistance3, testDriver.getTotalDistanceDriven());
 	}
 
 }
